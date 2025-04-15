5c2a0bbbd1da95778281a8f4ef770eba3917da61
pedro <pedro092692@gmail.com>
2025-04-15 21:17:32 +0200

    create dict with products info from amazon paapi like product-name product-iml-url and details

--- app/services/productService.py
+++ app/services/productService.py
@@ -17,7 +17,21 @@ class ProductService:
         )
 
     def search(self, query):
-        products = self.amazon.search_items(keywords='harry potter')
+        products = self.amazon.search_items(keywords="hidratacion piel grasosa", search_index="Beauty", item_count=10)
+        products_info = []
+        for item in products.items:
+            products_info.append({
+                'product_name': item.item_info.title.display_value,
+                'product_url': item.detail_page_url,
+                'image_url': item.images.primary.large.url,
+                'user_review': item.customer_reviews,
+                'product_price': item.offers.listings[0].price.amount,
+                'brand': item.item_info.by_line_info.brand.display_value
+            })
+            try:
+                products_info[-1]['product_features'] = item.item_info.features.display_values
+            except AttributeError:
+                products_info[-1]['product_features'] = []
 
-        print(products)
+        print(products_info)
 
